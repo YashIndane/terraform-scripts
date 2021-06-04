d4d1f7f0b40204fbfa06dfd5989e750de3a62a73
Yash Indane <yashindane46@gmail.com>
2021-06-04 23:05:17 +0530

    Add files via upload

---
+++ dynamodb-lock-remote-state/dynamo.tf
@@ -0,0 +1,35 @@
+provider "aws"{
+
+  region = "ap-south-1"
+  profile=""
+
+}
+
+resource "aws_dynamodb_table" "basic-dynamodb-table"{
+
+ name = "tfstate-lock-table"
+
+ read_capacity = 5
+ write_capacity = 5
+ hash_key = "LockID"
+
+ attribute{
+   name = "LockID"
+   type = "S"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "tfstate112"
+    key    = "mystate.tfstate"
+    region = "ap-south-1"
+    profile = ""
+   
+    dynamodb_table = "tfstate-lock-table"
+   
+ 
+  }
+}
+
+
---
+++ dynamodb-lock-remote-state/ec2.tf
@@ -0,0 +1,45 @@
+provider "aws"{
+
+  region = "ap-south-1"
+  profile=""
+
+}
+
+resource "aws_dynamodb_table" "basic-dynamodb-table"{
+
+ name = "tfstate-lock-table"
+
+ read_capacity = 5
+ write_capacity = 5
+ hash_key = "LockID"
+
+ attribute{
+   name = "LockID"
+   type = "S"
+  }
+}
+
+
+
+terraform {
+  backend "s3" {
+    bucket = "tfstate112"
+    key    = "mystate.tfstate"
+    region = "ap-south-1"
+    profile = ""
+    
+    dynamodb_table = "tfstate-lock-table"
+  
+  }
+}
+
+
+resource "aws_instance" "os34"{
+
+ ami = "ami-0de1d9ec99cd6630c"
+ instance_type = "t2.small"
+ tags = {
+     Name = "hadoop-os-20"
+
+   }
+}
